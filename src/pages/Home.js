3ea3b77377269f2e9c376363430036365ab5110e
Nitin Chandran Nair <16149618+NitinNair89@users.noreply.github.com>
2020-12-01 20:14:02 +0530

    Displayed TV show result & minor code refactor

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,58 @@
 import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import apiGet from '../misc/config';
 
 const Home = () => {
   // Hooks
   const [inputState, setInputState] = useState('');
+  const [tvShowInfo, setTVShowInfo] = useState(null);
 
-  // Function to handle onClick event of search button
-  // https://api.tvmaze.com/search/shows?q=
+  /**
+   * Function to handle onClick event of search button
+   */
   const handleOnClick = () => {
-    fetch(`https://api.tvmaze.com/search/shows?q=${inputState}`)
-      // convert response to JSON
-      .then(response => response.json())
-      // use converted JSON response
-      .then(result => {
-        console.log(result);
-      });
+    apiGet(`/search/shows?q=${inputState}`).then(result => {
+      setTVShowInfo(result);
+    });
   };
 
-  // Function to handle onChange event of search box
+  /**
+   * Function to handle onChange event of search box
+   * @param {*} event
+   */
   const handleOnChange = event => {
     setInputState(event.target.value);
   };
 
-  // Function to handle onKeyDown event of search box
+  /**
+   * Function to handle onKeyDown event of search box
+   * @param {*} event
+   */
   const handleOnKeyDown = event => {
     if (event.keyCode === 13) {
       handleOnClick();
     }
+  };
+
+  /**
+   * Function to display TV show's information
+   */
+  const renderTVShowInfo = () => {
+    if (tvShowInfo && tvShowInfo.length === 0) {
+      return <div>No results found!</div>;
+    }
+
+    if (tvShowInfo && tvShowInfo.length > 0) {
+      return (
+        <div>
+          {tvShowInfo.map(item => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+
+    return null;
   };
 
   return (
@@ -44,6 +70,7 @@ const Home = () => {
       <button type="button" onClick={handleOnClick}>
         Search
       </button>
+      {renderTVShowInfo()}
     </MainPageLayout>
   );
 };
