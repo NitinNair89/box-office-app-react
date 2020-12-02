225b38c00a5315f1f54998f033ae410254eccb89
Nitin Chandran Nair <16149618+NitinNair89@users.noreply.github.com>
2020-12-02 21:35:17 +0530

    Added conditional rendering and error handler

--- src/pages/ShowInfo.js
+++ src/pages/ShowInfo.js
@@ -6,12 +6,46 @@ const ShowInfo = () => {
   // Hooks
   const { showID } = useParams();
   const [showData, setShowData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setError] = useState(null);
 
   useEffect(() => {
-    apiGet(`/shows/${showID}?embed[]=seasons&embed[]=cast`).then(results => {
-      setShowData(results);
-    });
-  }, [showID]);
+    let isMounted = true;
+
+    apiGet(`/shows/${showID}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        if (isMounted) {
+          setShowData(results);
+          setIsLoading(false);
+        }
+      })
+      .catch(err => {
+        if (isMounted) {
+          setIsLoading(false);
+          setError(isError.message);
+        }
+      });
+
+    // Clean-up callback function
+    return () => {
+      isMounted = false;
+    };
+  }, [showID, isError]);
+
+  // Conditional rendering
+  if (isLoading) {
+    return <div>Loading TV show's information...</div>;
+  }
+
+  if (isError) {
+    return (
+      <div>
+        Error occurred.
+        <br />
+        {isError}
+      </div>
+    );
+  }
 
   return <div>This is show page</div>;
 };
