2a203ff8c121628de391938aaf4b40eb7c937a40
Nitin Chandran Nair <16149618+NitinNair89@users.noreply.github.com>
2020-12-05 16:40:16 +0530

    Defined theme and created styled components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import E404 from './pages/E404';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 import './index.css';
 import ShowInfo from './pages/ShowInfo';
 
+const theme = {
+  mainColors: {
+    blue: '#2400ff',
+    gray: '#c6c6c6',
+    dark: '#353535',
+  },
+};
+
 export default function App() {
   return (
-    <Switch>
-      <Route exact={true} path="/">
-        <Home />
-      </Route>
-      <Route exact={true} path="/starred">
-        <Starred />
-      </Route>
+    <ThemeProvider theme={theme}>
+      <Switch>
+        <Route exact={true} path="/">
+          <Home />
+        </Route>
+        <Route exact={true} path="/starred">
+          <Starred />
+        </Route>
 
-      <Route exact={true} path="/show/:showID">
-        <ShowInfo />
-      </Route>
-      <Route>
-        <E404 />
-      </Route>
-    </Switch>
+        <Route exact={true} path="/show/:showID">
+          <ShowInfo />
+        </Route>
+        <Route>
+          <E404 />
+        </Route>
+      </Switch>
+    </ThemeProvider>
   );
 }
